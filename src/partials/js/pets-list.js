56ce269c0da48859ad5414426f55e8dc02055a01
Ostapenko Bogdan <bogdan.ostapenko.97@gmail.com>
2025-12-14 11:01:46 +0200

    додав розмітку та css стилі для заголовку. також написав базові константи в js

---
+++ src/partials/js/pets-list.js
@@ -0,0 +1,6 @@
+BASE_URL = 'https://paw-hut.b.goit.study/';
+CATECORIES_URL = 'api/categories';
+ANIMALS_URL = 'api/animals';
+
+const categories = document.querySelector('.categories_list');
+const allAnimals = document.querySelector('.animals_list');
